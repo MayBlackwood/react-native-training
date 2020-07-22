03f58c75aded6d7f4176a2aa753f55332d92b850
MayBlackwood <ksenia.bobtsova@gmail.com>
2020-07-22 13:18:29 +0300

    Add styles to ListItem

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,29 +1,45 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-const ListItem = ({ item, index, drag, isActive }) => (
-  <TouchableOpacity
-    style={{
-      height: 100,
-      width: '100%',
-      backgroundColor: isActive
-        ? 'blue'
-        : `rgb(${Math.floor(Math.random() * 255)}, ${index * 5}, ${132})`,
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}
-    onLongPress={drag}
-  >
-    <Text
+const ListItem = ({
+  item: { username, firstname, lastname },
+  index,
+  drag,
+  isActive,
+}) => {
+  const getItemColor = (i) => (i % 2 === 0 ? '#7C05F2' : '#6204BF');
+
+  return (
+    <TouchableOpacity
       style={{
-        fontWeight: 'bold',
-        color: 'black',
-        fontSize: 32,
+        height: 100,
+        width: '100%',
+        backgroundColor: isActive ? '#F67280' : `${getItemColor(index)}`,
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
+      onLongPress={drag}
     >
-      {item.username}
-    </Text>
-  </TouchableOpacity>
-);
+      <Text
+        style={{
+          fontWeight: 'bold',
+          color: 'lightgrey',
+          fontSize: 32,
+        }}
+      >
+        {username}
+      </Text>
+      <Text
+        style={{
+          fontWeight: 'bold',
+          color: 'black',
+          fontSize: 20,
+        }}
+      >
+        {`${firstname} ${lastname}`}
+      </Text>
+    </TouchableOpacity>
+  );
+};
 
 export default ListItem;
